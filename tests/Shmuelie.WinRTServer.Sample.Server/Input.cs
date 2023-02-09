b8a65292020fa369606eb0a0cf748b569bb49eb5
Shmueli Englard <shmueli.yosef@englard.net>
2023-02-08 22:35:05 -0800

    Added example of using custom types in parameters and returns

---
+++ tests/Shmuelie.WinRTServer.Sample.Server/Input.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Shmuelie.WinRTServer.Sample.Interfaces;
+
+namespace Shmuelie.WinRTServer.Sample;
+
+[Guid("4F59AF92-A98D-4A20-8C8D-1C076647A6B0")]
+public sealed class Input : IInput
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
