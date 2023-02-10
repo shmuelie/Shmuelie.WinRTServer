fcc7b9b6b4cb1b86cbf7991b52a79bfc26af8fa6
Shmueli Englard <shmueli.yosef@englard.net>
2023-02-09 20:10:00 -0800

    Started creation of C++/WinRT client

---
+++ tests/Shmuelie.WinRTServer.Sample.CppConsoleClient/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include <winrt/Shmuelie.WinRTServer.Sample.Interfaces.h>
+#include <winrt/Shmuelie.WinRTServer.Sample.h>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Shmuelie::WinRTServer::Sample;
+using namespace Shmuelie::WinRTServer::Sample::Interfaces;
+
+IAsyncOperation<int> AsyncMain()
+{
+    RemoteThing remoteThing;
+
+    int32_t rem = remoteThing.Rem(5, 4);
+    printf("Rem: %i\n", rem);
+
+    printf("Delay Start...");
+    co_await remoteThing.DelayAsync(3000);
+    printf("Stop\n");
+
+    co_return 0;
+}
+
+int main()
+{
+    init_apartment();
+
+    return AsyncMain().get();
+}
