f4fa58054c716852548ecf0cd3269cc3e5f6b2e8
Shmueli Englard <shmueli.yosef@englard.net>
2023-02-26 15:21:38 -0800

    Added extension method to add factories with default constructors

--- src/Shmuelie.WinRTServer/WinRtServerExtensions.cs
+++ src/Shmuelie.WinRTServer/WinRtServerExtensions.cs
@@ -25,4 +25,23 @@ public static class WinRtServerExtensions
 
         return server.RegisterActivationFactory(new GeneralActivationFactory<T>());
     }
+
+    /// <summary>
+    /// Register an activation factory with the server.
+    /// </summary>
+    /// <typeparam name="T">The type of the factory to register.</typeparam>
+    /// <param name="server">The instance.</param>
+    /// <returns><see langword="true"/> if an instance of <typeparamref name="T"/> was registered; otherwise, <see langword="false"/>.</returns>
+    /// <remarks>Only one factory can be registered for a Activatable Class ID.</remarks>
+    /// <exception cref="ArgumentNullException"><paramref name="server"/> is <see langword="null"/>.</exception>
+    /// <exception cref="InvalidOperationException">The server is running.</exception>
+    public static bool RegisterActivationFactory<T>(this WinRtServer server) where T : BaseActivationFactory, new()
+    {
+        if (server is null)
+        {
+            throw new ArgumentNullException(nameof(server));
+        }
+
+        return server.RegisterActivationFactory(new T());
+    }
 }
