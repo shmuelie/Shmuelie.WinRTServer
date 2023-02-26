490e39a1077a7933d7cd456d4fcfdd82c7ab1038
Shmueli Englard <shmueli.yosef@englard.net>
2023-02-26 15:21:59 -0800

    Added delegate factories

---
+++ src/Shmuelie.WinRTServer/DelegateClassFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Shmuelie.WinRTServer;
+
+/// <summary>
+/// Delegate based class factory for .NET types.
+/// </summary>
+/// <typeparam name="T">Type the factory creates.</typeparam>
+/// <typeparam name="TInterface">Interface that <typeparamref name="T"/> implements.</typeparam>
+/// <seealso cref="BaseClassFactory"/>
+public sealed class DelegateClassFactory<T, TInterface> : BaseClassFactory where T : class, TInterface
+{
+    private readonly Func<T> factory;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DelegateClassFactory{T, TInterface}"/> class.
+    /// </summary>
+    /// <param name="factory">Delegate to create instances.</param>
+    public DelegateClassFactory(Func<T> factory)
+    {
+        this.factory = factory;
+    }
+
+    /// <inheritdoc/>
+    protected internal override Guid Clsid => typeof(T).GUID;
+
+    /// <inheritdoc/>
+    protected internal override Guid Iid => typeof(TInterface).GUID;
+
+    /// <inheritdoc/>
+    protected internal override object CreateInstance()
+    {
+        return factory();
+    }
+}
