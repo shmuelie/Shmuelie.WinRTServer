18ac7ea79d7f8e38e760ad71a1e0cace0ca3d7be
Shmueli Englard <shmueli.yosef@englard.net>
2023-12-15 21:59:08 -0800

    Stuff

--- tests/Shmuelie.WinRTServer.Sample.ServerNet6/Program.cs
+++ tests/Shmuelie.WinRTServer.Sample.ServerNet6/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Shmuelie.WinRTServer.Sample.Interfaces;
-
-[assembly: DisableRuntimeMarshalling]
 
 namespace Shmuelie.WinRTServer.Sample.Server;
 
