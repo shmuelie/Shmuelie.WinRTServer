f5447d30be7500d8e5f4679e010d5f230f33b1ad
Shmueli Englard <shmueli.yosef@englard.net>
2023-12-16 21:03:03 -0800

    Fix lookup for factories

---
+++ src/Shmuelie.WinRTServer/Windows/winrt/HSTRING.cs
@@ -0,0 +1,24 @@
+﻿using Windows.Win32.Foundation;
+using Windows.Win32.System.WinRT;
+using static Windows.Win32.PInvoke;
+
+namespace Shmuelie.Interop.Windows;
+
+internal static class HSTRING_
+{
+#if !NETSTANDARD
+    [System.Runtime.Versioning.SupportedOSPlatform("windows8.0")]
+#endif
+    public unsafe static string AsString(this HSTRING @this)
+    {
+        if (@this.IsNull)
+        {
+            return string.Empty;
+        }
+
+        uint characterCount;
+        PCWSTR characters = WindowsGetStringRawBuffer(@this, &characterCount);
+
+        return characters.ToString();
+    }
+}
