98f7bd6798ca91514d57498a29aaa84b1a2856ec
Shmueli Englard <shmueli.yosef@englard.net>
2024-08-24 20:50:50 -0700

    Update sample projects references and make sample server AOT

--- tests/Shmuelie.WinRTServer.Sample.ServerNet6/Input.cs
+++ tests/Shmuelie.WinRTServer.Sample.ServerNet6/Input.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 using Shmuelie.WinRTServer.Sample.Interfaces;
 
-namespace Shmuelie.WinRTServer.Sample;
+namespace Shmuelie.WinRTServer.Sample.Server;
 
 [Guid("2F8EDC40-C145-4128-B451-559D45CD9074")]
-public sealed class Input : IInput
+public sealed partial class Input : IInput
 {
     public string? Name { get; set; }
     public string? Description { get; set; }
--- tests/Shmuelie.WinRTServer.Sample.ServerNet6/Times.cs
+++ tests/Shmuelie.WinRTServer.Sample.ServerNet6/Times.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 using Shmuelie.WinRTServer.Sample.Interfaces;
 
-namespace Shmuelie.WinRTServer.Sample;
+namespace Shmuelie.WinRTServer.Sample.Server;
 
 [Guid("EC6A0FF9-BBF4-48EA-9BFA-DFAF84D4FAF8")]
-public sealed class Times : ITimes
+public sealed partial class Times : ITimes
 {
     public DateTimeOffset UtcNow { get; set; }
     public DateTimeOffset LocalNow { get; set; }
