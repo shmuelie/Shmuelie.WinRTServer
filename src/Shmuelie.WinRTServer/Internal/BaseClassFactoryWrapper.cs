d283accbf588ecf1f3eb7a7a2cf46142f07afa07
Shmueli Englard <shmueli.yosef@englard.net>
2024-08-28 21:25:58 -0700

    Simplify namespaces

--- src/Shmuelie.WinRTServer/Internal/BaseClassFactoryWrapper.cs
+++ src/Shmuelie.WinRTServer/Internal/BaseClassFactoryWrapper.cs
@@ -4,22 +4,23 @@ using System.Runtime.InteropServices.Marshalling;
 using Shmuelie.WinRTServer.Windows.Com;
 using IUnknown = Windows.Win32.System.Com.IUnknown;
 using static Windows.Win32.PInvoke;
+using Windows.Win32.Foundation;
 
 namespace Shmuelie.WinRTServer.Internal;
 
 [GeneratedComClass]
 internal partial class BaseClassFactoryWrapper(BaseClassFactory factory, ComWrappers comWrappers) : IClassFactory
 {
-    public unsafe global::Windows.Win32.Foundation.HRESULT CreateInstance(void* pUnkOuter, Guid* riid, void** ppvObject)
+    public unsafe HRESULT CreateInstance(void* pUnkOuter, Guid* riid, void** ppvObject)
     {
         if (pUnkOuter is not null)
         {
-            return global::Windows.Win32.Foundation.HRESULT.CLASS_E_NOAGGREGATION;
+            return HRESULT.CLASS_E_NOAGGREGATION;
         }
 
         if (!riid->Equals(IUnknown.IID_Guid) && !riid->Equals(factory.Iid))
         {
-            return global::Windows.Win32.Foundation.HRESULT.E_NOINTERFACE;
+            return HRESULT.E_NOINTERFACE;
         }
 
         try
@@ -33,7 +34,7 @@ internal partial class BaseClassFactoryWrapper(BaseClassFactory factory, ComWrap
             }
             else
             {
-                global::Windows.Win32.Foundation.HRESULT hr = (global::Windows.Win32.Foundation.HRESULT)Marshal.QueryInterface(unknown, ref *riid, out nint ppv);
+                HRESULT hr = (HRESULT)Marshal.QueryInterface(unknown, ref *riid, out nint ppv);
                 if (hr.Failed)
                 {
                     return hr;
@@ -45,12 +46,12 @@ internal partial class BaseClassFactoryWrapper(BaseClassFactory factory, ComWrap
         }
         catch (Exception e)
         {
-            return (global::Windows.Win32.Foundation.HRESULT)Marshal.GetHRForException(e);
+            return (HRESULT)Marshal.GetHRForException(e);
         }
-        return global::Windows.Win32.Foundation.HRESULT.S_OK;
+        return HRESULT.S_OK;
     }
 
-    public global::Windows.Win32.Foundation.HRESULT LockServer(global::Windows.Win32.Foundation.BOOL fLock)
+    public HRESULT LockServer(BOOL fLock)
     {
         try
         {
@@ -65,8 +66,8 @@ internal partial class BaseClassFactoryWrapper(BaseClassFactory factory, ComWrap
         }
         catch (Exception e)
         {
-            return (global::Windows.Win32.Foundation.HRESULT)Marshal.GetHRForException(e);
+            return (HRESULT)Marshal.GetHRForException(e);
         }
-        return global::Windows.Win32.Foundation.HRESULT.S_OK;
+        return HRESULT.S_OK;
     }
 }
