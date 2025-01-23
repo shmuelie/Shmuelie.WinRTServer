b734a699cc70407352aa8a1c978222e0e76dd597
Ragul <tragul95@gmail.com>
2025-01-23 16:45:51 +0530

    Changes to support WinRT server

--- src/Shmuelie.WinRTServer/Internal/BaseActivationFactoryWrapper.cs
+++ src/Shmuelie.WinRTServer/Internal/BaseActivationFactoryWrapper.cs
@@ -24,15 +24,11 @@ internal sealed partial class BaseActivationFactoryWrapper(BaseActivationFactory
         {
             object managedInstance = factory.ActivateInstance();
             unknown = comWrappers.GetOrCreateComInterfaceForObject(managedInstance, CreateComInterfaceFlags.None);
-            var hr = (HRESULT)Marshal.QueryInterface(unknown, in global::Windows.Win32.System.WinRT.IActivationFactory.IID_Guid, out nint ppv);
-            shouldReleaseUnknown = true;
-            if (hr.Failed)
-            {
-                return hr;
-            }
-            *instance = (void*)ppv;
+            *instance = (void*)unknown;
 
+            shouldReleaseUnknown = true;
             factory.OnInstanceCreated(managedInstance);
+            shouldReleaseUnknown = false;
         }
         catch (Exception e)
         {
