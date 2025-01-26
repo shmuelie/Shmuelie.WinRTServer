b7c2e40f2cfcd6c88d6a3f160162cf8cf7fdf573
Shmueli Englard <shmueli.yosef@englard.net>
2025-01-25 21:11:38 -0800

    Created initial version of lifecycle managers

---
+++ src/Shmuelie.WinRTServer/Lifecycle/ConditionalLifecycleManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Shmuelie.WinRTServer.Lifecycle;
+
+/// <summary>
+/// Manage the lifecycle of objects created by a server using a <see cref="ConditionalWeakTable{TKey, TValue}"/>.
+/// </summary>
+/// <seealso cref="IServer"/>
+public sealed class ConditionalLifecycleManager
+{
+    private sealed class Notifier(Action action)
+    {
+        ~Notifier() => action();
+    }
+
+    private readonly ConditionalWeakTable<object, Notifier> _notifiers = new();
+    private int _count;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConditionalLifecycleManager"/> class.
+    /// </summary>
+    /// <param name="server">The <see cref="IServer"/> to manage the lifecycle for.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="server"/> is <see langword="null"/>.</exception>
+    public ConditionalLifecycleManager(IServer server)
+    {
+        ArgumentNullException.ThrowIfNull(server);
+
+        server.InstanceCreated += OnInstanceCreated;
+    }
+
+    private void OnInstanceCreated(object? sender, InstanceCreatedEventArgs e)
+    {
+        Interlocked.Increment(ref _count);
+        _notifiers.Add(e.Instance, new Notifier(() =>
+        {
+            if (Interlocked.Decrement(ref _count) == 0)
+            {
+                Empty?.Invoke(this, EventArgs.Empty);
+            }
+        }));
+    }
+
+    /// <summary>
+    /// Occurs when all instances created by the server are collected.
+    /// </summary>
+    public event EventHandler? Empty;
+}
---
+++ src/Shmuelie.WinRTServer/Lifecycle/TimerLifecycleManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shmuelie.WinRTServer.Lifecycle;
+
+/// <summary>
+/// Manage the lifecycle of objects created by a server using a background thread.
+/// </summary>
+/// <seealso cref="IServer"/>
+/// <seealso cref="IDisposable"/>
+public sealed class TimerLifecycleManager : IDisposable
+{
+    private readonly BlockingCollection<object> _instances = [];
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TimerLifecycleManager"/> class.
+    /// </summary>
+    /// <param name="server">The <see cref="IServer"/> to manage the lifecycle for.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="server"/> is <see langword="null"/>.</exception>
+    public TimerLifecycleManager(IServer server)
+    {
+        ArgumentNullException.ThrowIfNull(server);
+
+        server.InstanceCreated += OnInstanceCreated;
+
+        _ = Task.Run(CheckingLoop);
+    }
+
+    /// <summary>
+    /// Gets or sets the time to wait at most for checking if all instances are alive still.
+    /// </summary>
+    public TimeSpan WaitTime
+    {
+        get;
+        set;
+    } = TimeSpan.FromMinutes(1);
+
+    /// <summary>
+    /// Occurs when all instances created by the server are collected.
+    /// </summary>
+    public event EventHandler? Empty;
+
+    private void OnInstanceCreated(object? sender, InstanceCreatedEventArgs e)
+    {
+        _instances.Add(e.Instance);
+    }
+
+    private void CheckingLoop()
+    {
+        LinkedList<WeakReference> references = new();
+        while (true)
+        {
+            if (_instances.TryTake(out object? instance, WaitTime))
+            {
+                references.AddLast(new WeakReference(instance));
+            }
+
+            GC.Collect();
+            for (LinkedListNode<WeakReference>? node = references.First; node != null; node = node.Next)
+            {
+                if (!node.Value.IsAlive)
+                {
+                    var previous = node.Previous;
+                    references.Remove(node);
+                    if (previous is null)
+                    {
+                        break;
+                    }
+                    node = previous;
+                }
+            }
+
+            if (references.Count == 0)
+            {
+                Empty?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        _instances.Dispose();
+    }
+}
