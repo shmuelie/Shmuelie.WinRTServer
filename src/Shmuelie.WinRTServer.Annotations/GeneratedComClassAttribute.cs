c289b1c902383c28c9c805e3410e7c6fc2c1a2f5
Shmueli Englard <shmueli.yosef@englard.net>
2026-02-08 13:39:59 -0800

    Start work on source generator

---
+++ src/Shmuelie.WinRTServer.Annotations/GeneratedComClassAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Shmuelie.WinRTServer.Annotations;
+
+/// <summary>
+/// Specifies that the attributed type will generate a COM factory class for the specified type when the specified interfaces are requested.
+/// </summary>
+/// <param name="type">The type the factory will produce.</param>
+/// <param name="interfaces">The interfaces the factory will support generating <paramref name="type"/> for.</param>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public sealed class GeneratedComClassAttribute(Type type, params Type[] interfaces) : Attribute
+{
+    public Type Type { get; } = type;
+    public Type[] Interfaces { get; } = interfaces;
+}
+
+/// <summary>
+/// Specifies that the attributed type will generate a COM factory class for the specified type.
+/// </summary>
+/// <typeparam name="T">The type the factory will produce.</typeparam>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class GeneratedComClassAttribute<T> : Attribute where T : class
+{
+}
+
+/// <summary>
+/// Specifies that the attributed type will generate a COM factory class for the specified type when the specified interface is requested.
+/// </summary>
+/// <typeparam name="T">The type the factory will produce.</typeparam>
+/// <typeparam name="TInterface">The interface the factory will support generating <typeparamref name="T"/> for.</typeparam>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public sealed class GeneratedComClassAttribute<T, TInterface> : Attribute where T : TInterface
+{
+}
