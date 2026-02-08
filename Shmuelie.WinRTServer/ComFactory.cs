8244348d40e4fe4ca51458fc7f8f5b0ab421d872
Shmueli Englard <shmueli.yosef@englard.net>
2026-02-08 13:18:43 -0800

    Start implementing COM Factory

---
+++ Shmuelie.WinRTServer/ComFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Com;
+
+namespace Shmuelie.WinRTServer;
+
+[GeneratedComClass]
+public abstract partial class ComFactory : IClassFactory
+{
+    public unsafe void CreateInstance([MarshalAs(UnmanagedType.Interface), Optional] object pUnkOuter, Guid* riid, [MarshalAs(UnmanagedType.Interface)] out object ppvObject)
+    {
+        if (pUnkOuter is not null)
+        {
+            throw new COMException() { HResult = HRESULT.CLASS_E_NOAGGREGATION };
+        }
+        ppvObject = CreateInstance(*riid);
+    }
+
+    protected abstract object CreateInstance(Guid riid);
+
+    void IClassFactory.LockServer(BOOL fLock)
+    {
+    }
+}
